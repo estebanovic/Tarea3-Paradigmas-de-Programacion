d98c1c2ebeac41bd3851f37cd2881c19fea87875
estebanovic <e.obilinovic@gmail.com>
2025-06-23 01:11:13 -0400

    menú principal

--- main.pl
+++ main.pl
@@ -1,15 +1,30 @@
 
 % Predicado principal
 main :-
-    write('Sistema de diagnóstico de Diabetes Tipo II'), nl,
-    write('-----------------------------------------'), nl, nl,
+    write('=== SISTEMA NUTRICIONAL - ==='), nl,nl,
     menu.
 
 % Menú de opciones
 menu :-
     repeat,
     write('Opciones:'), nl,
-    write('1. Realizar cuestionario'), nl,
-    write('2. Salir'), nl,
-    write('Seleccione una opción (1-2): '),
-    read(Opcion).
+    write('1. Calcular el peso ideal'), nl,
+    write('2. Calcular IMC tradicional'), nl,
+    write('3. CalcuCalcular IMC por edad y sexo'), nl,
+    write('4. Salir'), nl,
+    write('Seleccione una opción (1-4): '),
+    read(Opcion),
+    (Opcion == 1 -> calcularPesoIdeal, fail;
+     Opcion == 2 -> calcularIMC, fail;
+     Opcion == 3 -> calcularIMCEdadSexo, fail;
+     Opcion == 4 -> write('Gracias por usar el sistema. Adiós.'), nl, !, fail;
+     write('Opción inválida. Intente de nuevo.'), nl, fail).
+
+calcularPesoIdeal :-
+    write('//TODO: Peso ideal').
+
+calcularIMC :-
+    write('//TODO: IMC').
+
+calcularIMCEdadSexo :-
+    write('//TODO: IMC').
